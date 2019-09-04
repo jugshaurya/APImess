3efed3241274141540c9f5d10e79bbfc53b90593
Shaurya Singhal(jugshaurya) <shauryasinghal84@gmail.com>
2019-09-04 22:06:38 +0530

    :sparkles: Added top-headlines/category code

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ app.use(express.static('public'))
 
 
 const top_headline_URL = 'https://newsapi.org/v2/top-headlines'
-app.get('/api/news/top-headlines/:country', async (req, res) => {
+app.get('/api/news/top-headlines/country/:country', async (req, res) => {
   const country = req.params.country
-  if (country === 'in' || country === 'us'){
+  if (country === 'in' || country === 'us') {
     console.log(country)
     const news_response = await fetch(`${top_headline_URL}?country=${country}&apiKey=${process.env.NEWS_API_KEY}`)
     const news_json = await news_response.json()
     res.send(news_json)
-  }else{
+  } else {
+    res.send(403)
+    res.end()
+  }
+})
+
+app.get('/api/news/top-headlines/category/:category', async (req, res) => {
+  const category = req.params.category
+
+  if (category === 'business' || category === 'entertainment' || category === 'general' || category === 'health' || category === 'science' || category === 'sports' || category === 'technology') {
+    console.log(category)
+    const news_response = await fetch(`${top_headline_URL}?category=${category}&apiKey=${process.env.NEWS_API_KEY}`)
+    const news_json = await news_response.json()
+    res.send(news_json)
+  } else {
     res.send(403)
     res.end()
   }
@@ -24,5 +38,5 @@ app.get('/api/news/top-headlines/:country', async (req, res) => {
 
 const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-    console.log('Serving at port', PORT)    
+  console.log('Serving at port', PORT)
 })
--- public/news.js
+++ public/news.js
@@ -1,5 +1,4 @@
-const country_in = document.getElementById('in')
-// const country_us = document.getElementById('us')
+const result = document.getElementById('result')
 
 function generateCard(article) {
 
@@ -23,11 +22,33 @@ function generateCard(article) {
 
 $('form').submit((event) => {
   event.preventDefault()
+  const form_id = event.target.id
+  const form_children_length = event.target.length
+  const options = event.target.children
+  let option_id = ""
+  for (let i = 0; i < form_children_length - 1; i++) {
+    const first_child = options[i].children[0]
+    const isChecked = first_child.checked
+    if (isChecked) {
+      option_id = first_child.id
+      break
+    }
+  }
 
-  async function getDataByCountry(country_name) {
-    const result = document.getElementById('result')
+  switch (form_id) {
+    case 'country-news':
+      getNewsByCountry(option_id)
+      break;
+
+    case 'category-news':
+      console.log(option_id)
+      getNewsByCategory(option_id)
+      break;
+  }
+
+  async function getNewsByCountry(country_name) {
     result.innerHTML = ""
-    const news_response = await fetch(`/api/news/top-headlines/${country_name}`)
+    const news_response = await fetch(`/api/news/top-headlines/country/${country_name}`)
     const news_json = await news_response.json()
     const articles = news_json.articles
     articles.forEach(article => {
@@ -36,8 +57,16 @@ $('form').submit((event) => {
     })
   }
 
-  country_in.checked ?
-    getDataByCountry('in') :
-    getDataByCountry('us')
+
+  async function getNewsByCategory(category_name) {
+    result.innerHTML = ""
+    const news_response = await fetch(`/api/news/top-headlines/category/${category_name}`)
+    const news_json = await news_response.json()
+    const articles = news_json.articles
+    articles.forEach(article => {
+      const html = generateCard(article)
+      result.innerHTML += html
+    })
+  }
 
 })
